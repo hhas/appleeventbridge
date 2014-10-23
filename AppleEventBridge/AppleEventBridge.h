e221c4e14037e7bf3bdff989ca26d05bf308a27c
hhas <hhas@users.sourceforge.net>
2014-10-23 13:22:57 +0100

    Initial Commit

---
+++ AppleEventBridge/AppleEventBridge.h
@@ -0,0 +1,19 @@
+//
+//  AppleEventBridge.h
+//  AppleEventBridge
+//
+//  Created by Hamish Sanderson on 23/10/2014.
+//  Copyright (c) 2014 org.bitbucket.hhas. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AppleEventBridge.
+FOUNDATION_EXPORT double AppleEventBridgeVersionNumber;
+
+//! Project version string for AppleEventBridge.
+FOUNDATION_EXPORT const unsigned char AppleEventBridgeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppleEventBridge/PublicHeader.h>
+
+
